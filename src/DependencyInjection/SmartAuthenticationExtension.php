38e632d7c32fabc7aa3b42f1b9b2abd17a1cfde2
Mathieu DUCROT <mathieu.ducrot@pia-production.fr>
2018-04-06 11:44:01 +0200

    Add user encode password extension

---
+++ src/DependencyInjection/SmartAuthenticationExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Smart\AuthenticationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * @author Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
+ */
+class SmartAuthenticationExtension extends Extension implements PrependExtensionInterface
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('admin_extension.xml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('config.yml');
+    }
+}
