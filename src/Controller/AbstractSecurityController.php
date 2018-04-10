8b8003a1690b4f4ec226921b4980464aeec2bc52
Mathieu Ducrot <mathieu-ducrot@users.noreply.github.com>
2018-04-10 14:20:55 +0200

    Managing user profile (#3)
    * Adding user profile action

--- src/Controller/AbstractSecurityController.php
+++ src/Controller/AbstractSecurityController.php
@@ -2,7 +2,9 @@
 
 namespace Smart\AuthenticationBundle\Controller;
 
+use Smart\AuthenticationBundle\Security\Form\Type\UserProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -16,6 +18,18 @@ class AbstractSecurityController extends Controller
      * @var string
      */
     protected $context;
+
+    /**
+     * @param string      $id         The message id (may also be an object that can be cast to string)
+     * @param array       $parameters An array of parameters for the message
+     * @param string|null $domain     The domain for the message or null to use the default
+     *
+     * @return string
+     */
+    protected function translate($id, array $parameters = array(), $domain = null)
+    {
+        return $this->get('translator')->trans($id, $parameters, $domain);
+    }
     
     /**
      * @return Response
@@ -39,5 +53,43 @@ class AbstractSecurityController extends Controller
     private function getAuthenticationUtils()
     {
         return $this->get('security.authentication_utils');
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function profileAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserProfileType::class, $user, []);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render($this->context . '/security/profile.html.twig', [
+                'base_template' => $this->get('sonata.admin.pool')->getTemplate('layout'),
+                'admin_pool'    => $this->get('sonata.admin.pool'),
+                'form'          => $form->createView(),
+                'security_profile_url' => $this->generateUrl('admin_security_profile'),
+            ]);
+        }
+
+        if (null !== $user->getPlainPassword()) {
+            $encoder = $this->get('security.password_encoder');
+            $user->setPassword(
+                $encoder->encodePassword($user, $user->getPlainPassword())
+            );
+        }
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        $this->addFlash('success', $this->translate('profile_edit.processed', [], 'security'));
+
+        return $this->redirectToRoute('sonata_admin_dashboard');
     }
 }
---
+++ src/Security/Form/Type/UserProfileType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Smart\AuthenticationBundle\Security\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
+ */
+class UserProfileType extends AbstractType
+{
+    /**
+     * @inheritdoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'form.label_email'
+            ])
+            ->add('firstName', null, [
+                'label' => 'form.label_first_name'
+            ])
+            ->add('lastName', null, [
+                'label' => 'form.label_last_name'
+            ])
+            ->add(
+                'plainPassword',
+                RepeatedType::class,
+                [
+                    'type'               => 'password',
+                    'required'           => false,
+                    'first_options'      => ['label' => 'form.label_password'],
+                    'second_options'     => ['label' => 'form.label_password_confirmation'],
+                    'translation_domain' => $options['translation_domain'],
+                ]
+            );
+        ;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'translation_domain' => 'admin'
+            ])
+        ;
+    }
+}
